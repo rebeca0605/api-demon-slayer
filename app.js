b9083e0e6e0ce8a5cb7d6ac976bf4cc46962c4ee
REBECA GOMES DA SILVA <25132910@CFP127ALUNO.LOCAL>
2025-11-03 11:28:07 -0300

    Página de estilos adicionada

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ window.addEventListener('DOMContentLoaded', function () {
     })
 })
 
+window.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('estilos').addEventListener('click', function () {
+        window.location.href = './pagina4.html'
+    })
+})
+
 const galeriaDePersonagens = document.getElementById('charactersGallery')
 let currentPage = 1
 const totalPages = 9
@@ -20,7 +26,7 @@ async function buscarPersonagens(pagina = 1) {
         const response = await fetch(urlProxy)
         const personagens = await response.json()
 
-        return personagens.content || personagens
+        return personagens.content
     } catch (erro) {
         console.error('Erro ao buscar personagens:', erro)
         return []
@@ -51,7 +57,6 @@ async function exibirPersonagens() {
     })
 }
 
-// Eventos das setas já existentes
 document.getElementById('prevPage').addEventListener('click', () => {
     if (currentPage > 1) {
         currentPage--
@@ -70,4 +75,68 @@ document.getElementById('nextPage').addEventListener('click', () => {
     }
 })
 
+const galeriaDeEstilos = document.getElementById('stylesGallery')
+let pageCurrent = 1
+const pagesTotal = 9
+const elementsOnPages = 5
+
+async function buscarEstilos(pagina = 1) {
+
+    const urlOriginal = `https://www.demonslayer-api.com/api/v1/combat-styles?page=${pagina}`
+    const urlProxy = `https://api.allorigins.win/raw?url=${encodeURIComponent(urlOriginal)}`
+
+    try {
+        const response = await fetch(urlProxy)
+        const estilos = await response.json()
+
+        return estilos.content
+    } catch (erro) {
+        console.error('Erro ao buscar personagens:', erro)
+        return []
+    }
+}
+
+async function exibirEstilos() {
+    const styles = await buscarEstilos(pageCurrent)
+
+    galeriaDeEstilos.innerHTML = '' 
+
+    const stylesPages = styles.slice(0, elementsOnPages)
+
+    stylesPages.forEach(character => {
+        const card = document.createElement('div')
+        card.className = 'card'
+
+        const capa = document.createElement('img')
+        capa.src = character.img
+        capa.alt = character.name
+
+        const nome = document.createElement('p')
+        nome.textContent = character.name
+
+        card.appendChild(capa)
+        card.appendChild(nome)
+        galeriaDeEstilos.appendChild(card)
+    })
+}
+
+document.getElementById('prevPage').addEventListener('click', () => {
+    if (pageCurrent > 1) {
+        pageCurrent--
+        exibirEstilos()
+    } else {
+        alert('Você já está na primeira página!')
+    }
+})
+
+document.getElementById('nextPage').addEventListener('click', () => {
+    if (pageCurrent < totalPages) {
+        pageCurrent++
+        exibirEstilos()
+    } else {
+        alert('Você já está na última página!')
+    }
+})
+
 exibirPersonagens()
+exibirEstilos()
